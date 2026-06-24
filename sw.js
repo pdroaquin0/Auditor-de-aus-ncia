a0d94a6b8b3296cddd669bb7cd8c96e43a9ec988
pdroaquin0 <aquino.metrocasa@gmail.com>
2026-06-24 12:45:41 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE='metrocasa-acesso-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./favicon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()).catch(()=>{}));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+      return resp;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
